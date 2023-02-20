59f71863cc20a7a7196b9ed051a5822a93e30b63
Mila Data <miladata.sys@gmail.com>
2023-02-20 13:44:08 -0300

    text-area added

--- script.js
+++ script.js
@@ -4,23 +4,30 @@ const generateButton = document.getElementById('generate');
 const copyButton = document.getElementById('copy');
 const summaryDiv = document.getElementById('summary');
 
-// Generate a summarization and update the summaryDiv with the result
 function generateSummarization() {
   // Get the current value of the slider
   const summaryLength = slider.value;
 
+  // Get the input text from the textarea
+  const inputText = document.getElementById('input-text').value;
+
   // TODO: Generate the summarization using an API or algorithm
+  // You can use the OpenAI API to generate a summarization
 
   // Update the summaryDiv with the result
   summaryDiv.innerText = 'This is a sample summarization.';
 }
 
 // Add an event listener to the generateButton to generate a summarization on click
-generateButton.addEventListener('click', generateSummarization);
+generateButton.addEventListener('click', async () => {
+  try {
+    // Generate the summarization
+    await generateSummarization();
 
-// Add an event listener to the copyButton to copy the summarization to the clipboard on click
-copyButton.addEventListener('click', () => {
-  // Copy the text inside the summaryDiv to the clipboard
-  navigator.clipboard.writeText(summaryDiv.innerText);
-  alert('Copied to clipboard!');
+    // Copy the text inside the summaryDiv to the clipboard
+    await navigator.clipboard.writeText(summaryDiv.innerText);
+    alert('Copied to clipboard!');
+  } catch (error) {
+    console.error(error);
+  }
 });
